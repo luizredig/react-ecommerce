bb68dbfed7bddd101861c80fa6e06349023665e1
luizredig <luizredig@gmail.com>
2023-12-31 17:33:54 -0300

    feat: Categories and CategoryItem components were created and first banner home added

---
+++ src/app/(home)/components/categories.tsx
@@ -0,0 +1,17 @@
+import { prismaClient } from "@/lib/prisma";
+import CategoryItem from "./categorie-item";
+
+const Categories = async () => {
+  const categories = await prismaClient.category.findMany({});
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-4">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Categories;

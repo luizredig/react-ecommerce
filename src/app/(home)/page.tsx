35d5082594891032f5e8cec001bb172521236a83
luizredig <luizredig@gmail.com>
2024-01-02 14:57:45 -0300

    feat: ProductItem and ProductCarousel components were added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,35 @@
-"use client";
-
 import Image from "next/image";
 import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductCarousel from "./components/product-carousel";
 
-export default function Home() {
+export default async function Home() {
+  const productsOnSale = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
     <>
-      <div className="p-5">
+      <div>
         <Image
           src="/banner-home-01.png"
           height={0}
           width={0}
-          className="h-auto w-full"
+          className="h-auto w-full px-5"
           sizes="100vw"
-          alt="Highlight banner says: até 55% de desconto; só esse mês"
+          alt="Highlight banner says: até 55% de desconto. Só esse mês!"
         />
       </div>
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+
+      <div className="mt-8">
+        <ProductCarousel products={productsOnSale} />
       </div>
     </>
   );

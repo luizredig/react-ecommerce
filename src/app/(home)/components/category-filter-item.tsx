ceabff9b39ad20561155aaaa9f5a060f8795fdff
luizredig <luizredig@gmail.com>
2024-01-02 19:59:55 -0300

    feat: CATEGORY_ICON constant created

--- src/app/(home)/components/category-filter-item.tsx
+++ src/app/(home)/components/category-filter-item.tsx
@@ -1,36 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { CATEGORY_ICON } from "@/helpers/category-icon";
 import { Category } from "@prisma/client";
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareIcon,
-} from "lucide-react";
 
 interface CategoryFilterItemProps {
   category: Category;
 }
 
 const CategoryFilterItem = ({ category }: CategoryFilterItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon size={16}/>,
-    monitors: <MonitorIcon size={16}/>,
-    headphones: <HeadphonesIcon size={16}/>,
-    mousepads: <SquareIcon size={16}/>,
-    speakers: <SpeakerIcon size={16}/>,
-    mouses: <MouseIcon size={16}/>,
-  };
-
   return (
     <Button
       variant={"outline"}
       className="flex items-center justify-center gap-2 rounded-lg py-3"
     >
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
+      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
       <span className="text-xs font-bold">{category.name}</span>
     </Button>
   );

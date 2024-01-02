21e75cb24a16cb751cac030aaed32199588bf487
luizredig <luizredig@gmail.com>
2024-01-02 17:27:35 -0300

    feat: Footer component added

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 text-center">
+        <p>© 2024 Copyright <span className="font-semibold">redig</span> | Inspired in FWS Store</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;

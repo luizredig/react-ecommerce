0c9509d81467f5bc5a430a06dd1e9edda903d60b
luizredig <luizredig@gmail.com>
2024-01-02 15:18:24 -0300

    feat: second banner home added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,23 +14,34 @@ export default async function Home() {
 
   return (
     <>
-      <div>
-        <Image
-          src="/banner-home-01.png"
-          height={0}
-          width={0}
-          className="h-auto w-full px-5"
-          sizes="100vw"
-          alt="Highlight banner says: até 55% de desconto. Só esse mês!"
-        />
-      </div>
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        priority
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto. Só esse mês!"
+      />
+
       <div className="mt-8 px-5">
         <Categories />
       </div>
 
       <div className="mt-8">
+        <p className="mb-2 pl-5 font-bold uppercase">Ofertas</p>
         <ProductCarousel products={productsOnSale} />
       </div>
+
+      <Image
+        src="/banner-mouses.png"
+        height={0}
+        width={0}
+        priority
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </>
   );
 }

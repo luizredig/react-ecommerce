2efc2c5cae2256f1126341ac423eb309f877ecf7
luizredig <luizredig@gmail.com>
2024-01-02 17:11:06 -0300

    feat: dynamic images on ProductItem were added

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,18 +2,35 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import { prismaClient } from "@/lib/prisma";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = async ({ product }: ProductItemProps) => {
+  const productsImages = {
+    mouses: "/mx-master-3s.png",
+    monitors: "/monitor-redragon.png",
+    headphones: "/JBL_Quantum.png",
+    mousepads: "/logitech-mouse-pad.png",
+    keyboards: "/mx-keys-mini.png",
+    speakers: "/logitech-speakers.png",
+  };
+
+  const category = await prismaClient.category.findUniqueOrThrow({
+    where: {
+      id: product.categoryId,
+    },
+  });
+
   return (
     <>
       <div className="flex max-w-[156px] flex-col gap-4 ">
         <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
           <Image
-            src="/JBL_Quantum.png"
+            // src="/JBL_Quantum.png"
+            src={productsImages[category.slug as keyof typeof productsImages]}
             alt={product.name}
             height={0}
             width={0}
@@ -26,7 +43,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
 
           {product.discountPercentage > 0 && (
             <Badge className="absolute left-2 top-2 px-2 py-[2px]">
-              <ArrowDownIcon size={14}/>
+              <ArrowDownIcon size={14} />
               {product.discountPercentage} %
             </Badge>
           )}
